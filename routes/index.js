764aeb6e35e3ef2ea2b9980618865967232e0cfb
unknown <andresan2006@gmail.com>
2016-07-11 10:14:52 -0300

    Init character route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var express = require('express'),
 
 router.use('/users', require('./users'));
 
+router.use('/characters', require('./characters'));
+
 router.get('/', function(req, res) {
   res.render('index')
 });
