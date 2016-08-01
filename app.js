7e7af327cdcad121b516866c8019be2fc3d89050
andrehNSFW <andresan2006@gmail.com>
2016-08-01 10:59:42 -0300

    xOrigin configuration

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.use(bodyParser.json());
 
 //API Access Control to REST
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", config.xAcessControl);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
   next();
 });
 
